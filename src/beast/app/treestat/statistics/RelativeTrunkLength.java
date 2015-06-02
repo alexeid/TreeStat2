004bf1a0d90437669bfdd4e1d2f4743796ff0a66
Sasha <sasha.gavryushkina@auckland.ac.nz>
2015-06-03 11:45:04 +1200

    new statistics

---
+++ src/beast/app/treestat/statistics/RelativeTrunkLength.java
@@ -0,0 +1,23 @@
+package beast.app.treestat.statistics;
+
+import beast.app.treestat.statistics.AbstractTreeSummaryStatistic;
+import beast.app.treestat.statistics.SummaryStatisticDescription;
+import beast.evolution.tree.Tree;
+import beast.evolution.tree.TreeUtils;
+
+@SummaryStatisticDescription(
+        name = "Relative trunk length",
+        description = "The ratio of the trunk length to the tree length",
+        allowsNonultrametricTrees = true,
+        allowsPolytomies = false,
+        allowsUnrootedTrees = false
+)
+public class RelativeTrunkLength extends AbstractTreeSummaryStatistic<Double> {
+    public RelativeTrunkLength() {
+    }
+
+    public Double[] getSummaryStatistic(Tree tree) {
+        double treeLength = TreeUtils.getTreeLength(tree, tree.getRoot());
+        return new Double[]{Double.valueOf(TreeUtils.getTrunkLength(tree, tree.getRoot()) / treeLength)};
+    }
+}
