b142f102ecc7cde22e7113b961c264af0cbf2311
John <john@taysnet.com>
2023-06-13 15:25:24 +1000

    Added treestat2logger and new tree statistics
    TreeStat2Logger can log any tree statistic once added
    GetTypeChanges outputs the total type changes by type in a comma separated format
    SecondInternalNodeHeight gets the height of the second internal node

---
+++ src/treestat2/statistics/SecondInternalNodeHeight.java
@@ -0,0 +1,39 @@
+package treestat2.statistics;
+
+import beast.base.evolution.tree.Tree;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author John Tay
+ */
+
+@SummaryStatisticDescription(
+        name = "Second Internal Node Height",
+        description = "The height of the second internal node of the tree.",
+        allowsNonultrametricTrees = true,
+        allowsPolytomies = true,
+        allowsUnrootedTrees = false)
+
+public class SecondInternalNodeHeight extends AbstractTreeSummaryStatistic<Double> {
+    @Override
+    public Double[] getSummaryStatistic(Tree tree) {
+        double second_branch_length = 0.0;
+        if (tree.getRoot().getChildCount() < 3) {
+            List<Double> child_list = new ArrayList<Double>();
+            child_list.add(tree.getRoot().getChild(0).getLength());
+            child_list.add(tree.getRoot().getChild(1).getLength());
+            second_branch_length = Collections.min(child_list) ;
+        }
+        else {
+            List<Double> child_list = new ArrayList<Double>();
+            child_list.add(tree.getRoot().getChild(0).getLength());
+            child_list.add(tree.getRoot().getChild(1).getLength());
+            child_list.add(tree.getRoot().getChild(2).getLength());
+            second_branch_length = Collections.min(child_list) ;
+        }
+        return new Double[] { tree.getRoot().getHeight() - second_branch_length };
+    }
+}
